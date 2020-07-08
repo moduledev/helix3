7eb316dc78d416679f3080aab994e3d9e34eee21
moduledev <bayduzh89@gmail.com>
2020-07-08 11:24:09 +0300

    added dashboard index blade

---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.layouts.app')
+@section('content')
+    <div class="container-fluid">
+       <div class="row">
+           1
+       </div>
+    </div><!-- /.container-fluid -->
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('/', function () {
 Auth::routes(['register' => false]);
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::group(['prefix' => 'admin/dashboard','middleware' => ['auth:admin']], function () {
+    Route::get('/', 'Admin\DashboardController@index')->name('dashboard');
+});
+
