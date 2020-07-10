6e4875f4f788950fc38a21f06fa27ef15904a20b
moduledev <bayduzh89@gmail.com>
2020-07-10 11:44:58 +0300

    edit breadcrumbs config to change default template

--- resources/views/admin/dashboard/admin/index.blade.php
+++ resources/views/admin/dashboard/admin/index.blade.php
@@ -1,3 +1,20 @@
-@foreach($admins as $admin)
-    <li>{{$admin->name}}</li>
-@endforeach
+@extends('admin.layouts.app')
+@section('content-breadcrumbs')
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0 text-dark">Адміністратори</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                {{ Breadcrumbs::render('admins') }}
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+@endsection
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            1
+        </div>
+    </div><!-- /.container-fluid -->
+@endsection
--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -4,10 +4,10 @@ Breadcrumbs::for('dashboard', function ($trail) {
     $trail->push('Главная', route('admin.dashboard'));
 });
 
-//Breadcrumbs::for('admins', function ($trail) {
-//    $trail->parent('dashboard');
-//    $trail->push('Администраторы', route('admin.index'));
-//});
+Breadcrumbs::for('admins', function ($trail) {
+    $trail->parent('dashboard');
+    $trail->push('Администраторы', route('admin.index'));
+});
 //
 //Breadcrumbs::for('admin', function ($trail, $name) {
 //    $trail->parent('admins');
